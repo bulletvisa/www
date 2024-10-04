87a992dcd02d512b21ab7df7170a6d434ee42394
mynameisrohitpai <mynameisrohitpai@gmail.com>
2024-10-04 10:32:26 +0530

    updated logo and phone number

--- src/components/shared/app-logo.tsx
+++ src/components/shared/app-logo.tsx
@@ -13,8 +13,6 @@ export const AppLogo: React.FC<AppLogoProps> = ({ fill = "blue" }) => {
           ? "/assets/images/app-logo.png"
           : "/assets/images/app-logo-white.png"
       }
-      width={100}
-      height={100}
       alt={MAIN_WEBSITE_URL}
       className="w-16 md:w-24"
     />
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,7 @@
-export const MAIN_WEBSITE_URL = "https://bulletvisa.com";
+export const MAIN_WEBSITE_URL = "https://www.bulletvisa.com";
+
 export const ACCOUNT_WEBSITE_URL = "https://account.bulletvisa.com";
 
-export const CHAT_WITH_US_URL = "https://wa.me/+917304496915";
+export const CHAT_WITH_US_URL = "https://wa.me/+919004986915";
 
 export const TIMEZONE = "Asia/Kolkata";
